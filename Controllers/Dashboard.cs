b84b2a9f9df8033da519a58a9aa7586bf42c6d95
Vahe9955 <vahe99551@gmail.com>
2023-11-11 19:44:53 +0400

    fix

---
+++ Controllers/Dashboard.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskForLogix.Data;
+
+namespace TaskForLogix.Controllers
+{
+    public class Dashboard : Controller
+    {
+        private readonly ApplicationContext _context;
+        public Dashboard(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public async IActionResult Index()
+        {
+            var users = await _context.applicationUsers.ToListAsync();
+
+            return View(users);
+        }
+    }
+}
